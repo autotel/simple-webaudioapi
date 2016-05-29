5adb99c56b6e526da22c28960eee762ad095b095
Joaquin <joaquin@autotel.co>
2016-05-29 14:36:56 +0300

    initial push

---
+++ mouse.js
@@ -0,0 +1,34 @@
+Mouse=function(){
+  this.onMouseMove=function(event){
+    console.log(event);
+  }
+};
+mouse=new Mouse();
+//source: http://stackoverflow.com/questions/7790725/javascript-track-mouse-position
+(function() {
+    document.onmousemove = handleMouseMove;
+    function handleMouseMove(event) {
+        var dot, eventDoc, doc, body, pageX, pageY;
+
+        event = event || window.event; // IE-ism
+
+        // If pageX/Y aren't available and clientX/Y are,
+        // calculate pageX/Y - logic taken from jQuery.
+        // (This is to support old IE)
+        if (event.pageX == null && event.clientX != null) {
+            eventDoc = (event.target && event.target.ownerDocument) || document;
+            doc = eventDoc.documentElement;
+            body = eventDoc.body;
+
+            event.pageX = event.clientX +
+              (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+              (doc && doc.clientLeft || body && body.clientLeft || 0);
+            event.pageY = event.clientY +
+              (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+              (doc && doc.clientTop  || body && body.clientTop  || 0 );
+        }
+
+        // Use event.pageX / event.pageY here
+        mouse.onMouseMove(event);
+    }
+})();
